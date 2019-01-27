c024f41cb0e9d73180f81a7613cdb9e69fab10f2
Shaurya Jain <shauryajain2109@gmail.com>
2019-01-27 12:36:01 -0800

    Add files via upload

---
+++ Speech/proximity.lua
@@ -0,0 +1,56 @@
+-- nwHacks 2019
+-- Shaurya Jain & Akshay Khandelwal
+
+scriptId = 'in.shauryajain.proximity'
+-- domain
+
+scriptTitle = "Proximity"
+-- name of the script
+
+scriptDetailsUrl = "shauryajain.in"
+-- website homepage for the product
+
+function onPoseEdge(pose, edge)
+	if(pose == "doubleTap") then
+		myo.unlock("timed")
+	elseif (pose == "rest") then
+		-- Do nothing
+	else
+    	if(edge == "on") then
+	    	-- myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
+	    	sendMessage(pose .. "")
+			myo.lock()
+		end
+	end
+
+end
+
+function onPeriodic()
+end
+
+function onForegroundWindowChange(app, title)
+    -- myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+    return true
+end
+
+function activeAppName()
+    return "Output Everything"
+end
+
+function onActiveChange(isActive)
+    -- myo.debug("onActiveChange")
+end
+
+function sendMessage(msg)
+	-- myo.debug(msg)
+	if (msg == "fist") then
+		myo.keyboard("1", "press")
+	elseif (msg == "waveOut") then
+		myo.keyboard("2", "press")
+	elseif (msg == "waveIn") then
+		myo.keyboard("3", "press")
+	elseif (msg == "fingersSpread") then
+		myo.keyboard("4", "press")
+	end
+end
+
